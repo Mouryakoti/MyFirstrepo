147497a9a18a5059a011365d6bc998d0b54a3352
Mouryakoti <158200852+Mouryakoti@users.noreply.github.com>
2025-08-03 16:57:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-# This program adds two numbers
+# Python program to find the largest number among the three input numbers
 
-num1 = 1.5
-num2 = 6.3
+# change the values of num1, num2 and num3
+# for a different result
+num1 = 10
+num2 = 14
+num3 = 12
 
-# Add two numbers
-sum = num1 + num2
+# uncomment following lines to take three numbers from user
+#num1 = float(input("Enter first number: "))
+#num2 = float(input("Enter second number: "))
+#num3 = float(input("Enter third number: "))
 
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number is", largest)
